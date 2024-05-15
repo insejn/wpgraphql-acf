f4a2d49209ced5a79822a19d922c572dbbddbabe
Piotr Konicki <piotrkonicki@gmail.com>
2024-05-15 13:11:00 +0100

    init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-graphql/wpgraphql-acf',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '04e62e893b878189fed6658a729a96468a948b15',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'appsero/client' => array(
+            'pretty_version' => 'v1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'd110c537f4ca92ac7f3398eee67cc6bdf506a4fb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../appsero/client',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-graphql/wpgraphql-acf' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '04e62e893b878189fed6658a729a96468a948b15',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
